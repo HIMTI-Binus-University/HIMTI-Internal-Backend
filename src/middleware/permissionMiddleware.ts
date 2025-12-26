9082b81e66317be3ea4092d2641098214965f5c7
daffafayyaz <daffafyyaz@gmail.com>
2025-12-27 00:14:59 +0700

    feature: added permssion middleware [HIMTI-104]

---
+++ src/middleware/permissionMiddleware.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express';
+import { PrismaClient } from '@/generated/prisma/client.js';
+
+const prisma = new PrismaClient();
+
+export const requirePermission = (permissionName: string) => {
+   return async (_req: Request, res: Response, next: NextFunction) => {
+      const user = res.locals.user;
+      const userWithPermission = await prisma.user.findFirst({
+         where: {
+            id: user.id,
+            userHasRoles: {
+               some: {
+                  role: {
+                     roleHasPermissions: {
+                        some: {
+                           permission: {
+                              name: permissionName,
+                           },
+                        },
+                     },
+                  },
+               },
+            },
+         },
+      });
+
+      console.log(userWithPermission);
+
+      if (!userWithPermission) {
+         return res.status(403).json({
+            success: false,
+            message: `You do not have the permission to access this feature.`,
+         });
+      }
+
+      next();
+   };
+};
