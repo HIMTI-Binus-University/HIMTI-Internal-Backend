ed93d78b995c1e09c04cc002a3b73ff42919c85f
daffafayyaz <daffafyyaz@gmail.com>
2025-12-28 15:14:27 +0700

    fix: added some cors

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ app.use(
          'http://72.62.122.54:8001', // IP VPS (Akses html testing via IP)
          'http://72.62.122.54.nip.io:8001', // Domain nip.io (Akses Frontend via Domain)
          'http://localhost:3000',
+         'http://localhost:8000',
       ],
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
