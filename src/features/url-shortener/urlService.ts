a5788fc928a753cfdb1be3ac3d91af997de40317
daffafayyaz <daffafyyaz@gmail.com>
2025-12-28 22:22:32 +0700

    fix: zod changes #3

--- src/features/url-shortener/urlService.ts
+++ src/features/url-shortener/urlService.ts
@@ -22,7 +22,7 @@ class UrlService {
             shortCode: payload.shortCode,
             // createdBy: user.name,
             createdBy: 'Admin',
-            expiresAt: payload.expiresAt ?? null,
+            expiresAt: payload.expresAt ?? null,
          },
       });
    }
