9fc7d20ecc7639a1465b38e0941c719cfa5b523b
daffafayyaz <daffafyyaz@gmail.com>
2026-03-19 23:40:04 +0700

    feat: create subevent

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,7 @@ import urlRoutes from '@/features/url-shortener/urlRoutes.js';
 import blasterRoutes from '@/features/email-blaster/blasterRoutes.js';
 import registRoutes from '@/features/registration/registRoutes.js';
 import eventRoutes from '@/features/events/eventRoutes.js';
+import subEventRoutes from '@/features/sub-events/subEventRoutes.js';
 
 const router: Router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/url', urlRoutes);
 router.use('/email-blast', blasterRoutes);
 router.use('/registration', registRoutes);
 router.use('/event', eventRoutes);
+router.use('/sub-event', subEventRoutes);
 
 export default router;
