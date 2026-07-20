958fd9eb0b18835f964ede31911b1efe58846219
Joshua Alexander Larido <joshualarido@gmail.com>
2026-07-20 18:23:57 +0700

    fix: merge conflicts

--- src/features/events/eventController.ts
+++ src/features/events/eventController.ts
@@ -2,33 +2,6 @@ import { Request, Response } from 'express';
 import { CreateEventSchema } from './eventSchema.js';
 import { eventService } from './eventService.js';
 
-<<<<<<< Updated upstream
-=======
-export const getEvents = async (req: Request, res: Response) => {
-   const query = GetEventSchema.parse(req.query);
-   const userData = res.locals.user;
-   const result = await eventService.getEvents(query, userData);
-
-   res.status(200).json({
-      msg: 'success',
-      ...result,
-   });
-};
-
-export const getEventById = async (req: Request, res: Response) => {
-   const { id } = req.params;
-   const result = await eventService.getEventById(
-      id as string,
-      res.locals.user,
-   );
-
-   res.status(200).json({
-      msg: 'success',
-      data: result,
-   });
-};
-
->>>>>>> Stashed changes
 export const createEvent = async (req: Request, res: Response) => {
    const data = req.body;
    const userData = res.locals.user;
